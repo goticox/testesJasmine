bf7bb7e4d8ba544b9468fbd3b5c497fbd751dc56
Gustavo <gumesx@gmail.com>
2022-03-17 21:06:59 -0300

    aula01-&-aula02 Jasmine com Nodejs e Nodemon

---
+++ spec/anagram-spec.js
@@ -0,0 +1,23 @@
+//tdd
+var isAnagram = require('../anagram');
+
+describe('Teste Anagram', function(){
+    it('quando for verdadeiro "abc" e "cba"', function(){
+        expect(isAnagram('abc','cba')).toEqual(true);
+    })
+    it('quandofor verdadeiro "Amor" e "Roma"',function(){
+        expect(isAnagram('Amor','Roma')).toEqual(true);
+    })
+    it('se é verdade que existem duas strings vazias', function(){
+        expect(isAnagram('','')).toEqual(true);
+    })
+    it('é verdade quando "132" é igual a "312"', function(){
+        expect(isAnagram('132','312')).toEqual(true);
+    })
+    it('é verdade quando 0.12 é igual "102"',function(){
+        expect(isAnagram(0.12,'102')).toEqual(true);
+    })
+    it('é falso quando 012 não é igual a 102',function(){
+        expect(isAnagram(012,102)).toEqual(false);
+    })
+})
