39b1b17fc5e9d41f203eefe0bd7146012fa79c1d
unknown <you@example.com>
2022-03-23 20:48:29 -0300

    Update aula 23/03

--- spec/matchers-spec.js
+++ spec/matchers-spec.js
@@ -7,9 +7,9 @@ describe("Suíte de testes do toBe", function () {
     var obj2 = { 'valor': valorBooleano };
 //===valor e tipo quem faz é (toBe) e == apenas compara valores quem faz é (toEqual)
     it("deve validar o uso do matcher 'toBe'", function () {
-        expect(valorBoleano).toBe(true);
-        expect(valorBoleanoCopia).toBe(valorBooleano);
-        expect(valorBoleano).not.toBe(valorBooleanoTexto);
+        expect(valorBooleano).toBe(true);
+        expect(valorBooleanoCopia).toBe(valorBooleano);
+        expect(valorBooleano).not.toBe(valorBooleanoTexto);
         expect(valorBooleanoTexto).toBe("true");
         expect(obj1).not.toBe(obj2);
 
@@ -26,10 +26,10 @@ describe("Suite de testes do toEqual", function(){
     var obj2 = { 'valor': valorBooleano };
 
     it("deve validar o uso do matcher 'toEqual'",function() {
-        expect(valorBoleano).toEqual(true);
-        expect(valorBoleano).toEqual(valorBooleano2);
-        expect(valorBoleanoCopiar).toEqual(valorBooleano);
-        expect(valorBoleano).not.toEqual(valorBooleanoTexto);
+        expect(valorBooleano).toEqual(true);
+        expect(valorBooleano).toEqual(valorBooleano2);
+        expect(valorBooleanoCopiar).toEqual(valorBooleano);
+        expect(valorBooleano).not.toEqual(valorBooleanoTexto);
         expect(valorBooleanoTexto).toEqual("true");
         expect(obj1).not.toEqual(obj2);
     });
